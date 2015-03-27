8f48d878e3cfa678fac9efe5b4adbc0ad589c792
Rogelio Gudino <cananito@gmail.com>
2015-03-27 12:30:12 -0700

    Carthage output.

---
+++ Carthage/Build/iOS/LittleConsole.framework/Headers/LittleConsole.h
@@ -0,0 +1,19 @@
+//
+//  LittleConsole.h
+//  LittleConsole
+//
+//  Created by Rogelio Gudino on 3/26/15.
+//  Copyright (c) 2015 Rogelio Gudino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LittleConsole.
+FOUNDATION_EXPORT double LittleConsoleVersionNumber;
+
+//! Project version string for LittleConsole.
+FOUNDATION_EXPORT const unsigned char LittleConsoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LittleConsole/PublicHeader.h>
+
+
